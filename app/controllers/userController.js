3cdf54d329bbc4d9cbf9b18434886d8de8b53638
SoleneSol <so.buono@gmail.com>
2025-11-18 11:20:51 +0100

    update cookies for logout

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -137,8 +137,16 @@ const userController = {
 
     // Se déconnecter
     logout: async (req, res) => {
-        res.clearCookie("access_token");
-        res.clearCookie("refresh_token");
+        res.clearCookie("access_token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        });
+        res.clearCookie("refresh_token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        });
         res.json({ message: "Déconnexion effectuée" });
     },
 
